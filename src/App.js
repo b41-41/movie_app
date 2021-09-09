98142f9f91862132cf77476ce21c586e18eda0ba
b41-41 <41@b41.kr>
2021-09-09 16:12:27 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './movies';
 
-function App() {
-  return (
-    <div className="App"></div>
+class App extends React.Component {
+state = {
+  isLoading: true,
+  movie: []
+};
+
+getMovies = async () => {
+  const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+  this.setState({movies, isLoading: false })
+}
+
+componentDidMount() {
+  this.getMovies() ;
+};
+
+render() {
+  const { isLoading, movies } = this.state;
+  return ( 
+    <section class="container">
+      {isLoading 
+        ? <div class="loader">
+          <span class="Loader_text"></span>
+        </div>
+        :<div class="movies">
+          {movies.map(movie => (
+          <Movie 
+            key={movie.id}
+            id={movie.id} 
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.medium_cover_image}
+          />
+          )
+          )}
+        </div> 
+        }
+    </section>
   );
+}
 }
 
 export default App;
