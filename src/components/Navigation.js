6298b6af494305ab32f899fff36c6c17a03fb606
b41-41 <41@b41.kr>
2021-10-04 14:49:49 +0900

    fix divbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,11 +3,20 @@ import { Link } from 'react-router-dom';
 import './navigation.css';
 
 function Navigation() {
-    return <div className="navigation">
-        <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/about">About</Link></li>
-        </ul>
+    return <div className="navBar">
+        <div className="navBar__logo">
+            Movie App
+        </div>
+
+        <div className="navBar__navigation">
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/about">About</Link></li>
+            </ul>
+        </div>
+
+        <div className="navBar__icon">
+        </div>
     </div>
 }
 
